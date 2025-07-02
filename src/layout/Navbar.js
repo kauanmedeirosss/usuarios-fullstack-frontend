46a781b30dcda6d4275e36f62919799026adf7ab
lorimedeiros <lorimedeirosc@gmail.com>
2025-07-02 20:58:00 -0300

    1.4 Redirecionamentos usando Routes

--- src/layout/Navbar.js
+++ src/layout/Navbar.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Navbar() {
     return (
@@ -20,9 +21,9 @@ export default function Navbar() {
                         <span className="navbar-toggler-icon"></span>
                     </button>
 
-                    <button className="btn btn-outline-light">
+                    <Link className="btn btn-outline-light" to="/adduser">
                         Add User
-                    </button>
+                    </Link>
                 </div>
             </nav>
         </div>
