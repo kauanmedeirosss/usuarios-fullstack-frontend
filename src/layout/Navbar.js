1fef4e382eaad9ca184b1e0f7044e36f33624be6
lorimedeiros <lorimedeirosc@gmail.com>
2025-08-03 21:23:30 -0300

    Final

--- src/layout/Navbar.js
+++ src/layout/Navbar.js
@@ -6,9 +6,9 @@ export default function Navbar() {
         <div>
             <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
                 <div className="container-fluid">
-                    <a className="navbar-brand" href="#">
-                        A Incrível Aplicação Full Stack de Teste
-                    </a>
+                    <Link className="navbar-brand" to="/">
+                        Aplicação CRUD Full Stack
+                    </Link>
                     <button
                         className="navbar-toggler"
                         type="button"
